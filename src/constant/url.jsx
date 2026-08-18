f34284b4bf812bad6c88e2f870ecd9fc563356a9
Siddiq Ibrahim <siddiqibrahim55555@gmail.com>
2026-08-18 17:57:23 +0530

    URL Change

--- src/constant/url.jsx
+++ src/constant/url.jsx
@@ -1,2 +1,2 @@
-// export const baseUrl = "http://localhost:5000"
-export const baseUrl = "x-clone-backend-sable.vercel.app"
+export const baseUrl = "http://localhost:5000"
+// export const baseUrl = "x-clone-backend-sable.vercel.app"
